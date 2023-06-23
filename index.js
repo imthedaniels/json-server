5820cea0d2bb2e68d2bdefa619e24fa62c8cd0cf
Alex Gomes <alexsgomes0704@gmail.com>
2023-06-23 10:05:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
 
-const headers = {
-  headers: [
-    {key: 'Access-Control-Allow-Origin', value: '*'},
-    {key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'},
-    {key: 'Access-Control-Allow-Credentials', value: 'true'}
-]
-}
+module.exports = {
+  trailingSlash: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [];
+  }
+};
 
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
@@ -18,7 +30,6 @@ server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-server.use(headers())
 
 const PORT = 8000
 
