b77f06278fe86789769bcc70f6736a1a0bffb706
Alex Gomes <alexsgomes0704@gmail.com>
2023-06-23 10:18:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,15 @@ const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
 
-module.exports = {
-  trailingSlash: false,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [];
-  }
-};
-
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
+
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 server.use(cors())
 server.use(jsonServer.bodyParser)
