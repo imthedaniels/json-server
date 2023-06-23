c0b97cc46a524636f66ac095caeb0bc6d00bf6f9
Alex Gomes <alexsgomes0704@gmail.com>
2023-06-23 13:56:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-module.exports = () => {
-  const app= express();
-
-  app.use(cors());
-}
-
+server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
