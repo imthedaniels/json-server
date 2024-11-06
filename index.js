b13003fdf8f2bcbc266de9362e3acfb1a52913cb
Daniel Martins <imthedaniels@gmail.com>
2024-11-05 22:37:48 -0300

    commita ai jesus

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); // Your database file
+const middlewares = jsonServer.defaults();
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
-const corsOpts = {
-  origin: '*',
-
-  methods: [
-    'GET',
-    'POST',
-    'PUT'
-  ],
-
-  allowedHeaders: [
-    'Content-Type'
-  ]
-}
-
-server.use(cors(corsOpts))
-
-
-server.use(cors())
+// Add custom middleware for CORS
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any origin
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
 
-const PORT = 8000
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
